36efb5313df15a8570b2e0c87ecf9d3c445ef15e
n-h-a <hanikki811@gmail.com>
2026-09-13 23:20:33 -0700

    Add: partition-labels - submission-2

---
+++ Data Structures & Algorithms/partition-labels/submission-2.py
@@ -0,0 +1,36 @@
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        last_idx = {}
+        for i in range(len(s)):
+            last_idx[s[i]] = i
+
+        res = []
+        smallest, furthest = 0, 0
+        for i in range(len(s)):
+            furthest = max(furthest, last_idx[s[i]])
+
+            if i == furthest:
+                res.append(furthest + 1 - smallest)
+                smallest = furthest + 1
+                continue
+        
+        return res
+
+# GOAL
+# ======
+# Given a string, return a list of substring sizes
+# s.t. no letters appear in more than one substring.
+
+
+# OBSERVATIONS
+# ======
+# * For each letter, we care about its latest occurence.
+
+# BRUTE FORCE
+# ======
+# For each letter, create a list of intervals.
+# Merge overlapping intervals. Return sizes.
+
+# IDEA
+# ======
+# Use two pointer greedy approach.
