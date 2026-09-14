712a1141abcb099825c77a1d3cdc42a331ebc0f8
n-h-a <hanikki811@gmail.com>
2026-09-14 14:58:30 -0700

    Add: n-queens - submission-0

---
+++ Data Structures & Algorithms/n-queens/submission-0.py
@@ -0,0 +1,47 @@
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        col = set()
+        posDiag = set() # (r + c)
+        negDiag = set() # (r - c)
+
+        res = []
+        board = [["."] * n for i in range(n)]
+
+        def backtrack(r):
+            if r == n:
+                copy = ["".join(row) for row in board]
+                res.append(copy)
+                return        
+
+            for c in range(n):
+                if c in col or (r + c) in posDiag or (r - c) in negDiag:
+                    continue
+
+                col.add(c)
+                posDiag.add(r + c)
+                negDiag.add(r - c)
+                board[r][c] = "Q"
+                
+                backtrack(r + 1)
+
+                col.remove(c)
+                posDiag.remove(r + c)
+                negDiag.remove(r - c)
+                board[r][c] = "."
+
+        backtrack(0)
+        return res
+
+# GOAL
+# =======
+# Given an int n, return all possible layouts s.t.
+# we can place n queens on an n x n board without
+# them attacking each other.
+
+# OBSERVATIONS
+# =======
+# * Impossible with a 2 x 2 and a 3 x 3.
+
+# IDEA
+# =======
+
